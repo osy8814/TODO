9d53095ddf8a04d4aa9a3e2b3aa32c1249de71dc
haribo14 <edcosy@gmail.com>
2022-10-22 22:17:32 +0900

    fix : 폰트사이트 기억기능 추가
    LocalStroage에 폰트사이즈 기억

--- js/note.js
+++ js/note.js
@@ -4,9 +4,21 @@ const ClearBtn = document.querySelector(`.clearNote`);
 const SizeUpBtn = document.querySelector('.fa-plus-square');
 const SizeDownBtn = document.querySelector('.fa-minus-square');
 const SizeinitBtn = document.querySelector('.fa-sync-alt');
-let DefaultFontSize = 3;
 
+const KET_FONTSIZE = "noteFontSize";
 const KEY_NOTECONTENT = "noteContent";
+
+let DefaultFontSize;
+
+const SavedNoteFontSize = localStorage.getItem(KET_FONTSIZE);
+if (SavedNoteFontSize !== null) {
+    DefaultFontSize = parseInt(SavedNoteFontSize);
+
+} else {
+    DefaultFontSize = 3;
+}
+Note.style.fontSize = `${DefaultFontSize}vmin`;
+
 
 function saveNote() {
     localStorage.setItem(KEY_NOTECONTENT, Note.value);
@@ -24,8 +36,10 @@ ClearBtn.addEventListener("click", clearNote);
 
 const SizeController = {
     SizeUp: function () {
+
         DefaultFontSize = DefaultFontSize + 0.5;
         Note.style.fontSize = `${DefaultFontSize}vmin`;
+        localStorage.setItem(KET_FONTSIZE, DefaultFontSize);
     },
     SizeDown: function () {
         DefaultFontSize = DefaultFontSize - 0.5;
@@ -33,10 +47,12 @@ const SizeController = {
             DefaultFontSize = 1;
         }
         Note.style.fontSize = `${DefaultFontSize}vmin`;
+        localStorage.setItem(KET_FONTSIZE, DefaultFontSize);
     },
     SizeInit: function () {
         DefaultFontSize = 3;
         Note.style.fontSize = `${DefaultFontSize}vmin`;
+        localStorage.setItem(KET_FONTSIZE, DefaultFontSize);
     }
 
 }
